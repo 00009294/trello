937194e1d3da8fcc6d629614ec8629939fec03ae
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2024-12-05 15:25:43 +0500

    some changes

--- Domain/Entities/Attachements/Exceptions/NotFoundAttachementException.cs
+++ Domain/Entities/Attachements/Exceptions/NotFoundAttachementException.cs
@@ -5,7 +5,7 @@ namespace Domain.Entities.Attachements.Exceptions
     public class NotFoundAttachementException : Xeption
     {
         public NotFoundAttachementException(int id)
-           : base(message: $"Couldn't find attachement with id: {id}.")
+           : base(message: $"Couldn't find attachement with id: {id}. ")
         { }
     }
 }
