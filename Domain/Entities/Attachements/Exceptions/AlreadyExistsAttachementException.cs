8cf921bb8438b589c0b402ebcb91c34ecb0b6da4
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2024-12-05 15:25:08 +0500

    Clean up unnecessary files and update .gitignore

---
+++ Domain/Entities/Attachements/Exceptions/AlreadyExistsAttachementException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace Domain.Entities.Attachements.Exceptions
+{
+    public class AlreadyExistsAttachementException : Xeption
+    {
+        public AlreadyExistsAttachementException(Exception innerException)
+            : base(message: "An attachement already exists! ", innerException)
+        { }
+    }
+}
---
+++ Domain/Entities/Attachements/Exceptions/NotFoundAttachementException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace Domain.Entities.Attachements.Exceptions
+{
+    public class NotFoundAttachementException : Xeption
+    {
+        public NotFoundAttachementException(int id)
+           : base(message: $"Couldn't find attachement with id: {id}.")
+        { }
+    }
+}
