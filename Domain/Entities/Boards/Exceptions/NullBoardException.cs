6801b64d5574812d48c64d949597625538a0b6bb
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2024-12-05 15:33:46 +0500

    some changes

--- Domain/Entities/Boards/Exceptions/NullBoardException.cs
+++ Domain/Entities/Boards/Exceptions/NullBoardException.cs
@@ -5,6 +5,6 @@ namespace Domain.Entities.Exceptions;
 public class NullBoardException : Xeption
 {
     public NullBoardException()
-        : base(message: "Board is null.")
+        : base(message: "A board is null.")
     { }
 }
